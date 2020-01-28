36e777d96c9eb09c270b5f3e0af05a10b0aed037
Christian Prior <cprior@bink.com>
2020-01-28 18:03:47 +0000

    encrypt payment card fields before POST

--- request_data/payment_card.py
+++ request_data/payment_card.py
@@ -3,7 +3,18 @@ import time
 import uuid
 from enum import IntEnum
 
-from settings import fake
+from shared_config_storage.credentials.encryption import RSACipher
+
+from request_data.membership_plan import ClientBundleIDs
+from settings import fake, VAULT_URL, VAULT_TOKEN
+
+FIELDS_TO_ENCRYPT = (
+    'first_six_digits',
+    'last_four_digits',
+    'month',
+    'year',
+    'hash'
+)
 
 
 class PaymentProvider(IntEnum):
@@ -12,7 +23,7 @@ class PaymentProvider(IntEnum):
     VISA = "42"
 
 
-def generate_random():
+def generate_unencrypted_random():
     random_four = str(random.randint(1000, 9999))
     card_bin = random.choice(list(PaymentProvider))
     return {
@@ -36,6 +47,22 @@ def generate_random():
             ]
         },
     }
+
+
+def generate_random():
+    pcard = generate_unencrypted_random()
+    rsa = RSACipher(VAULT_TOKEN, VAULT_URL, ClientBundleIDs.BARCLAYS)
+    for field in FIELDS_TO_ENCRYPT:
+        cred = pcard['card'].get(field)
+        if not cred:
+            raise ValueError(f"Missing credential {field}")
+        try:
+            encrypted_val = rsa.encrypt(cred)
+        except Exception as e:
+            raise ValueError(f"Value: {cred}") from e
+        pcard['card'][field] = encrypted_val
+
+    return pcard
 
 
 def generate_static():

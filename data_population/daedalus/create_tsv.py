150457197a369bf7151001b01277d0e9c893ae1c
kaziz <kaziz@bink.com>
2020-02-14 11:32:15 +0000

    Add service tsv creation

--- data_population/daedalus/create_tsv.py
+++ data_population/daedalus/create_tsv.py
@@ -10,12 +10,14 @@ TSV_PATH = f"{os.path.dirname(__file__)}/tsv"
 LOAD_START_ID = 2000000
 BULK_SIZE = 1000
 CLIENT_FIXTURES = [CLIENT_ONE, CLIENT_TWO, CLIENT_RESTRICTED]
+SERVICE_COUNT = 180_000_000
 
 
 class Files(str, Enum):
     CHANNEL = "channel.tsv"
     MEMBERSHIP_PLAN = "membership_plan.tsv"
     CHANNEL_WHITELIST = "channel_membership_plan_whitelist.tsv"
+    SERVICE = "service.tsv"
 
 
 def tsv_path(file_name):
@@ -57,6 +59,29 @@ def create_tsv():
             whitelist_list.append(create_data.channel_whitelist(whitelist_id, client_fixture, plan_id))
 
     write_to_tsv(Files.CHANNEL_WHITELIST, whitelist_list)
+
+    client_fixtures = [CLIENT_ONE, CLIENT_TWO]
+    fixture_count = len(client_fixtures)
+    total_services_remaining = SERVICE_COUNT
+    print("creating services")
+
+    while total_services_remaining > 0:
+        services = []
+        batch_counter = BULK_SIZE
+        while batch_counter > 0 and total_services_remaining > 0:
+            service_id = SERVICE_COUNT - total_services_remaining
+            client_fixture = client_fixtures[service_id % fixture_count]
+            services.append(
+                create_data.service(
+                    client_fixture,
+                    service_id=service_id,
+                    email=f"performanc{service_id}@test.locust"
+                )
+            )
+            batch_counter -= 1
+            total_services_remaining -= 1
+
+        write_to_tsv(Files.SERVICE, services)
 
     end = time.perf_counter()
     print(f"Elapsed time: {end - start}")

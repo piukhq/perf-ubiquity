4c708b8d2ef81447380372539c2d110bff271388
Christian Prior <cprior@bink.com>
2020-02-26 14:44:59 +0000

    add scheme_category creation for hermes database

--- data_population/hermes/upload_tsv.sh
+++ data_population/hermes/upload_tsv.sh
@@ -6,3 +6,4 @@ psql -h 127.0.0.1 -p 5432 --user "postgres" -d mcwallet_hermes -c "\copy user_cl
 psql -h 127.0.0.1 -p 5432 --user "postgres" -d mcwallet_hermes -c "\copy scheme_scheme FROM '$TSV_FOLDER/scheme_scheme.tsv' DELIMITER E'\t'"
 psql -h 127.0.0.1 -p 5432 --user "postgres" -d mcwallet_hermes -c "\copy scheme_schemecredentialquestion FROM '$TSV_FOLDER/scheme_schemecredentialquestion.tsv' DELIMITER E'\t'"
 psql -h 127.0.0.1 -p 5432 --user "postgres" -d mcwallet_hermes -c "\copy scheme_schemebundleassociation FROM '$TSV_FOLDER/scheme_schemebundleassociation.tsv' DELIMITER E'\t'"
+psql -h 127.0.0.1 -p 5432 --user "postgres" -d mcwallet_hermes -c "\copy scheme_category FROM '$TSV_FOLDER/scheme_category.tsv' DELIMITER E'\t'"

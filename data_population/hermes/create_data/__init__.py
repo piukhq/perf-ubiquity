d91936a1800340fb54c22c2bc611c5ca25e92026
Christian Prior <cprior@bink.com>
2020-03-11 11:55:21 +0000

    remove import check on organisational imports

--- data_population/hermes/create_data/__init__.py
+++ data_population/hermes/create_data/__init__.py
@@ -1,9 +1,10 @@
-from data_population.hermes.create_data.associations import card_service_link, pll_link
-from data_population.hermes.create_data.channel import (organisation, client_application, client_application_bundle,
-                                                        channel_scheme_whitelist)
-from data_population.hermes.create_data.membership_card import membership_card, card_number_answer, postcode_answer
-from data_population.hermes.create_data.membership_plan import (category, membership_plan, card_no_question,
-                                                                postcode_question, scheme_consent,
-                                                                third_party_consent_link, scheme_image)
-from data_population.hermes.create_data.payment_card import payment_card
-from data_population.hermes.create_data.service import service
+from data_population.hermes.create_data.associations import card_service_link, pll_link  # noqa
+from data_population.hermes.create_data.channel import (organisation, client_application,  # noqa
+                                                        client_application_bundle, channel_scheme_whitelist)  # noqa
+from data_population.hermes.create_data.membership_card import (membership_card, card_number_answer,  # noqa
+                                                                postcode_answer)  # noqa
+from data_population.hermes.create_data.membership_plan import (category, membership_plan, card_no_question,  # noqa
+                                                                postcode_question, scheme_consent,  # noqa
+                                                                third_party_consent_link, scheme_image)  # noqa
+from data_population.hermes.create_data.payment_card import payment_card  # noqa
+from data_population.hermes.create_data.service import service  # noqa

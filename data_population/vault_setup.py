ccb9cb0ca1c16994e7aef42f07030e10b7100f2f
Christian Prior <cprior@bink.com>
2020-03-13 17:26:27 +0000

    fix vault setup for new vault data stuctures

--- data_population/vault_setup.py
+++ data_population/vault_setup.py
@@ -1,31 +1,30 @@
 import requests
 
-from data_population.fixtures import CLIENT_ONE, CLIENT_TWO, CLIENT_RESTRICTED
+from data_population.fixtures import ALL_CLIENTS
 from request_data.membership_plan import ClientBundleIDs
 from settings import VAULT_TOKEN, VAULT_URL, CHANNEL_VAULT_PATH
-
-CLIENT_FIXTURES = [CLIENT_ONE, CLIENT_TWO, CLIENT_RESTRICTED]
 
 
 def setup_vault():
     headers = {"X-Vault-Token": VAULT_TOKEN}
     vault_channels = requests.get(f"{VAULT_URL}/v1/secret{CHANNEL_VAULT_PATH}", headers=headers).json()['data']
-    # barclays_jwt_secret = vault_channels[ClientBundleIDs.BARCLAYS]['jwt_secret']
+
+    test_keys_url = f"{VAULT_URL}/v1/secret/data/{ClientBundleIDs.BARCLAYS}"
+    test_payment_card_keys = requests.get(test_keys_url, headers=headers).json()['data']
 
     all_channels = {}
-    for client_fixture in CLIENT_FIXTURES:
+    for client_fixture in ALL_CLIENTS:
         all_channels[client_fixture["bundle_id"]] = {
-            "jwt_secret": "testsecret"
+            "jwt_secret": "testsecret",
+            "private_key": test_payment_card_keys['data']['private_key'],
+            "public_key": test_payment_card_keys['data']['public_key']
         }
 
     if not all(item in vault_channels.items() for item in all_channels.items()):
         all_channels.update(vault_channels)
         requests.post(f"{VAULT_URL}/v1/secret{CHANNEL_VAULT_PATH}", headers=headers, json=all_channels)
 
-    test_keys_url = f"{VAULT_URL}/v1/secret/data/{ClientBundleIDs.BARCLAYS}"
-    test_payment_card_keys = requests.get(test_keys_url, headers=headers).json()['data']
-
-    for client in CLIENT_FIXTURES:
+    for client in ALL_CLIENTS:
         keys_url = f"{VAULT_URL}/v1/secret/data/{client['bundle_id']}"
         resp = requests.get(keys_url, headers=headers)
         if resp.status_code == 404:

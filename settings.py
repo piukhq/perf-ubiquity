b32ba1af910bb8e156df131c6fabebd79f676d08
Francesco Milani <fmilani@bink.com>
2021-01-13 13:35:10 +0200

    fix wrongly modified default

--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ VAULT_TOKEN = env_var("VAULT_TOKEN")
 DB_USER = env_var("DB_USER", "postgres")
 DB_PASSWORD = env_var("DB_PASSWORD", "")
 DB_HOST = env_var("DB_HOST", "127.0.0.1")
-DB_PORT = env_var("DB_PORT", "5556")
+DB_PORT = env_var("DB_PORT", "5432")
 HERMES_DB = env_var("HERMES_DB", "hermes")
 HADES_DB = env_var("HADES_DB", "hades")
 

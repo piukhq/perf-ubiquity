f012fd5e57c9db0f228055400564b9134ee37221
Azhar Khan <92938104+AzharKh4n@users.noreply.github.com>
2022-02-09 17:31:38 +0000

    switching to delete endpoint

--- locust_angelia/locustfile.py
+++ locust_angelia/locustfile.py
@@ -39,16 +39,16 @@ class WebsiteUser(HttpUser):
         "delete_join": 0,  # Should be less than total joins (or will 404)
         "delete_loyalty_card": 0,  # Should be less than total loyalty_cards added (or will 404)
         # --PAYMENT_ACCOUNTS--
-        "post_payment_account": 4,  # Single and Multiuser (1 each) - Adds 1 card
-        "patch_payment_account": 4,  # Will 404 if no post_payment_account
-        "delete_payment_account": 3,  # Will 404 if > post_payment_account
+        "post_payment_account": 0,  # Single and Multiuser (1 each) - Adds 1 card
+        "patch_payment_account": 0,  # Will 404 if no post_payment_account
+        "delete_payment_account": 0,  # Will 404 if > post_payment_account
         # --WALLET--
         "get_wallet": 0,
         "get_wallet_overview": 0,
         "get_wallet_loyalty_card": 0,  # Will 404 if no loyalty cards
         # --USER--
         "post_email_update": 0,
-        "delete_me": 0,
+        "delete_me": 1,
         # --SPECIAL--
         "stop_locust_after_test_suite": 1,
     }

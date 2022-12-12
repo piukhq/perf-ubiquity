9c84c20cd7bf81ac94cb30d03684cc7d37abd9cd
Christian Prior <chris.prior.uk@gmail.com>
2022-12-12 13:29:53 +0000

    updated history body fixtures

--- data_population/fixtures/membership_card.py
+++ data_population/fixtures/membership_card.py
@@ -2,23 +2,17 @@ import json
 
 STATIC_MCARD_HISTORY_BODY = json.dumps(
     {
-        "id": 41305,
-        "order": 0,
+        "id": 240123,
+        "order": 1,
         "scheme": 1,
-        "status": 0,
         "barcode": "",
-        "created": "2021-01-19T13:55:46.101443Z",
-        "updated": "2021-01-19T13:55:46.101556Z",
-        "balances": {},
-        "user_set": [123],
-        "vouchers": {},
+        "created": "2022-12-09T11:19:12.837011Z",
+        "updated": "2022-12-12T10:12:15.171270Z",
         "join_date": None,
-        "link_date": None,
-        "pll_links": [],
+        "link_date": "2022-12-09T11:19:27.852030Z",
         "is_deleted": False,
         "card_number": "",
-        "main_answer": "robojeff@testbink.com",
-        "transactions": [],
-        "formatted_images": {},
+        "alt_main_answer": "pytest+auto@testbink.com",
+        "originating_journey": 2
     }
 )

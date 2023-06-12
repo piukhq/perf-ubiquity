1c859503033446ab429c5efb04166f40b1b88525
Mick Latham <6615305+Mick-Cadac@users.noreply.github.com>
2023-06-12 16:34:09 +0100

    Fix encryption field to be string (#148)

--- data_population/encryption.py
+++ data_population/encryption.py
@@ -46,11 +46,11 @@ def get_aes_key(filename: str):
     return aes_key.encode()
 
 
-def decrypt_credentials(credentials: str) -> dict:
+def decrypt_credentials(credential: str) -> dict:
     aes = AESCipher(get_aes_key(os.path.join(settings.SECRETS_LOCATION, "aes-keys")))
-    return json.loads(aes.decrypt(credentials.replace(" ", "+")))
+    return json.loads(aes.decrypt(credential.replace(" ", "+")))
 
 
-def encrypt_credentials(credentials: dict) -> str:
+def encrypt_credentials(credential: str) -> str:
     aes = AESCipher(get_aes_key(os.path.join(settings.SECRETS_LOCATION, "aes-keys")))
-    return aes.encrypt(json.dumps(credentials).encode()).decode("utf-8")
+    return aes.encrypt(credential).decode("utf-8")

a34eac03821c22f3c57a988fa577f1a27c3e8e75
Mick Latham <6615305+Mick-Cadac@users.noreply.github.com>
2023-06-30 15:26:54 +0100

    Populate merchant identifer field for harmonia perf testing (#151)

--- data_population/row_generation/create_mcard.py
+++ data_population/row_generation/create_mcard.py
@@ -55,7 +55,7 @@ def membership_card(card_id, scheme_id, transaction_total):
         json.dumps([]),  # pll_links
         "{}",  # formatted_images
         5,  # originating_journey
-        "",  # merchant_identifier
+        card_number,  # merchant_identifier
     ]
 
 
